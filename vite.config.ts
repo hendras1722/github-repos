0c0f89d286b4ef0ff7c941fe9297a3c5e0e28bff
hendras1722 <muhsyahendraa1722@gmail.com>
2025-06-11 07:34:31 +0700

    fix: update asset paths in index.html and remove base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: '/github-repos/',
+  base: '',
   server: {
     proxy: {
       '/api': {
